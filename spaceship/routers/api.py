95585bcd3788986810f84882ed0019ca6881d8cc
Yana <y.papish14@gmail.com>
2025-05-23 16:09:32 +0300

    add numpy dependence and matrix multiplication endpoint

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -6,3 +6,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrix')
+def matrix_multiply():
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": np.dot(matrix_a, matrix_b).tolist()
+    }
